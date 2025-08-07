c6bae3a55defa4594a4ddd8a6fcd774c856456d4
Kevine_dev <kevinarson_1996@yahoo.fr>
2025-08-08 01:03:41 +0200

    add component header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header className="text-center py-6 md:py-10 border-b border-gray-700">
+      <h1 className="text-4xl md:text-5xl font-bold text-white">
+        J'ai Lu Les <span className="text-indigo-400">CGU</span>
+      </h1>
+      <p className="text-gray-400 mt-2 text-lg">(Enfin, presque...)</p>
+    </header>
+  );
+}
+
+export default Header;
