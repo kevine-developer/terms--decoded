aeda4a25ef23d3f820d997a1ae2f9aefeb2ad4b4
Kevine_dev <kevinarson_1996@yahoo.fr>
2025-08-08 15:36:27 +0200

    Add cursor pointer for button action and button copie

--- src/components/OutputDisplay.tsx
+++ src/components/OutputDisplay.tsx
@@ -1,5 +1,4 @@
-
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 interface OutputDisplayProps {
   outputText: string;
@@ -10,19 +9,41 @@ interface CopyIconProps {
   className?: string;
 }
 
-const CopyIcon  = ({ className} : CopyIconProps) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
-    </svg>
+const CopyIcon = ({ className }: CopyIconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+    />
+  </svg>
 );
 
-const CheckIcon  = ({ className }: CopyIconProps) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
-    </svg>
+const CheckIcon = ({ className }: CopyIconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M5 13l4 4L19 7"
+    />
+  </svg>
 );
 
-const OutputDisplay = ({ outputText, error }: OutputDisplayProps ) => {
+const OutputDisplay = ({ outputText, error }: OutputDisplayProps) => {
   const [copied, setCopied] = useState(false);
 
   useEffect(() => {
@@ -44,19 +65,25 @@ const OutputDisplay = ({ outputText, error }: OutputDisplayProps ) => {
   return (
     <div className="relative h-full w-full flex flex-col">
       <div className="flex justify-between items-center p-4 border-b border-gray-700">
-        <h3 className="text-lg font-medium text-gray-300">Traduction en clair</h3>
+        <h3 className="text-lg font-medium text-gray-300">
+          Traduction en clair
+        </h3>
         {outputText && !error && (
-            <button
+          <button
             onClick={handleCopy}
-            className={`flex items-center gap-2 text-sm py-1 px-3 rounded-md transition-colors duration-200 ${
-                copied
-                ? 'bg-green-600 text-white'
-                : 'bg-gray-600 hover:bg-gray-500 text-gray-200'
+            className={`flex items-center  gap-2 text-sm py-1 px-3 rounded-md transition-colors duration-200 ${
+              copied
+                ? "bg-green-600 text-white"
+                : "bg-gray-600 hover:bg-gray-500 text-gray-200 cursor-pointer"
             }`}
-            >
-            {copied ? <CheckIcon className="w-4 h-4" /> : <CopyIcon className="w-4 h-4" />}
-            {copied ? 'Copié !' : 'Copier'}
-            </button>
+          >
+            {copied ? (
+              <CheckIcon className="w-4 h-4" />
+            ) : (
+              <CopyIcon className="w-4 h-4" />
+            )}
+            {copied ? "Copié !" : "Copier"}
+          </button>
         )}
       </div>
 
@@ -68,13 +95,31 @@ const OutputDisplay = ({ outputText, error }: OutputDisplayProps ) => {
           </div>
         )}
         {!error && outputText && (
-          <p style={{ whiteSpace: 'pre-wrap' }}>{outputText}</p>
+          <p style={{ whiteSpace: "pre-wrap" }}>{outputText}</p>
         )}
         {!hasContent && (
-           <div className="h-full flex flex-col items-center justify-center text-center text-gray-500">
-             <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 mb-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" /></svg>
-            <p className="font-medium">Le résultat de la traduction apparaîtra ici.</p>
-            <p className="text-sm">Prêt à transformer du charabia en quelque chose d'utile (ou de drôle) ?</p>
+          <div className="h-full flex flex-col items-center justify-center text-center text-gray-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-12 w-12 mb-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={1}
+                d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+              />
+            </svg>
+            <p className="font-medium">
+              Le résultat de la traduction apparaîtra ici.
+            </p>
+            <p className="text-sm">
+              Prêt à transformer du charabia en quelque chose d'utile (ou de
+              drôle) ?
+            </p>
           </div>
         )}
       </div>
