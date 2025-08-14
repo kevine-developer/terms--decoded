a08cf8dd2f324a0db02a711a8c489727ade4e3cc
Kevine_dev <kevinarson_1996@yahoo.fr>
2025-08-15 00:48:08 +0200

    Refactor color scheme to use emerald shades across components

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,21 +1,24 @@
-
 interface ActionButtonProps {
   onClick: () => void;
   disabled?: boolean;
   children: React.ReactNode;
 }
 
-function ActionButton ( { onClick, disabled = false, children }: ActionButtonProps ) {
+function ActionButton({
+  onClick,
+  disabled = false,
+  children,
+}: ActionButtonProps) {
   return (
     <button
       type="button"
       onClick={onClick}
       disabled={disabled}
-      className="w-full bg-purple-600 hover:bg-purple-700 text-white font-bold py-3 px-4 rounded-lg shadow-md  transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-900 focus:ring-indigo-500 disabled:bg-gray-500 disabled:cursor-not-allowed cursor-pointer disabled:shadow-none"
+      className="w-full bg-emerald-600 hover:bg-emerald-700 text-white font-bold py-3 px-4 rounded-lg shadow-md  transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-900 focus:ring-indigo-500 disabled:bg-gray-500 disabled:cursor-not-allowed cursor-pointer disabled:shadow-none"
     >
       {children}
     </button>
-  )
+  );
 }
 
-export default ActionButton
+export default ActionButton;
--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -5,8 +5,8 @@ interface ctaProps {
 
 function Cta({ link, title }: ctaProps) {
   return (
-    <div className="mb-2 p-3 bg-gradient-to-r from-purple-900/30 to-blue-900/30 border border-purple-500/20 rounded-lg backdrop-blur-sm">
-      <p className="text-sm  text-purple-200 flex  flex-col md:flex-row items-center text-center gap-2">
+    <div className="mb-2 p-3 bg-gradient-to-r from-emerald-900/30 to-blue-900/30 border border-emerald-500/20 rounded-lg backdrop-blur-sm">
+      <p className="text-sm  text-emerald-200 flex  flex-col md:flex-row items-center text-center gap-2">
         <span>
           👋 <strong>Hey&nbsp;!</strong> Envie d’en savoir plus sur moi et mes
           projets&nbsp;?
@@ -15,7 +15,7 @@ function Cta({ link, title }: ctaProps) {
           href={link}
           target="_blank"
           rel="noopener noreferrer"
-          className="font-semibold text-purple-400"
+          className="font-semibold text-emerald-400"
           aria-label="Découvrir mon profil, s'ouvre dans un nouvel onglet"
         >
           {title}
